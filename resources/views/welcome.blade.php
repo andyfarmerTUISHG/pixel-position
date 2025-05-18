880a6c9d54b57a846fbc92d5dd8e94426ab2a600
Andy Farmer <akf0676@sky.com>
2025-05-18 14:54:58 +0100

    Add Tags Section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,4 +7,19 @@
             <x-jobcard />
         </div>
     </section>
+    <section>
+        <x-section-heading>Tags</x-section-heading>
+        <div class="mt-6 space-x-1">
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+    </section>
 </x-layout>

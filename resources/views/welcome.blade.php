a374ccdb7d5dd00e6cdd27931752acb7f70f294e
Andy Farmer <akf0676@sky.com>
2025-05-18 15:00:00 +0100

    Addn Tags to main welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,31 @@
-<x-layout>
-    <section>
-        <x-section-heading>Featured Jobs</x-section-heading>
-        <div class="grid lg:grid-cols-3 gap-8">
-            <x-jobcard />
-            <x-jobcard />
-            <x-jobcard />
-        </div>
-    </section>
-    <section>
-        <x-section-heading>Tags</x-section-heading>
-        <div class="mt-6 space-x-1">
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-        </div>
-    </section>
+<x-layout >
+    <div class="space-y-10">
+
+        <section>
+            <x-section-heading>Featured Jobs</x-section-heading>
+            <div class="grid lg:grid-cols-3 gap-8">
+                <x-jobcard />
+                <x-jobcard />
+                <x-jobcard />
+            </div>
+        </section>
+        <section>
+            <x-section-heading>Tags</x-section-heading>
+            <div class="mt-6 space-x-1">
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+            </div>
+        </section>
+        <section>
+            <x-section-heading>Recent Jobs</x-section-heading>
+        </section>
+    </div>
 </x-layout>

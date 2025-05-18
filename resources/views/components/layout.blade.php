ddc2c8d6b2b26bd119ae5769c4624008680788e2
Andy Farmer <akf0676@sky.com>
2025-05-18 14:27:25 +0100

    Add some base tailwind styling to layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body>
-    <div>
-        <nav>
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4
+        border-b border-white/10
+        ">
             <div>
                 <a href="/">
                     <img
@@ -19,7 +20,7 @@
                         />
                 </a>
             </div>
-            <div>
+            <div class="space-x-6 font-bold">
                 <a href="/">Jobs</a>
                 <a href="/careers">Careers</a>
                 <a href="/salaries">Salaries</a>
@@ -29,7 +30,7 @@
                 <a href="/post-a-job">Post a Job</a>
             </div>
         </nav>
-        <main>
+        <main class="mt-10 max-w-[986px] mx-auto">
             {{ $slot }}
         </main>
 </body>

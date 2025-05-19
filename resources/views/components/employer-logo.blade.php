8a95d60cdc3a6fa94d80105ae3fd947f36c247ad
Andy Farmer <akf0676@sky.com>
2025-05-19 10:05:19 +0100

    Update to Components and layouts before making Dynamic

---
+++ resources/views/components/employer-logo.blade.php
@@ -0,0 +1,8 @@
+@props([
+    'width' => 90,
+])
+<img
+    src="https://placehold.co/{{ $width }}"
+    alt="Company Logo"
+    class="rounded-full"
+ >
---
+++ resources/views/components/jobcard-wide.blade.php
@@ -0,0 +1,27 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+    <div class="flex-1 flex flex-col">
+        <a href="/company/1" class="self-start text-sm text-gray-700">
+            Laracasts
+        </a>
+
+            <h3 class="font-bold text-lg
+            group-hover:text-blue-800
+            transition-colors duration-300
+            ">Senior Laravel Developer</h3>
+            {{-- <p>Remote</p> --}}
+            <p class="text-sm text-gray-700 mt-auto">$100,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+    </div>
+
+
+</x-panel>

fa83877f897db526c75608fba6c3a1c241960c17
Andy Farmer <akf0676@sky.com>
2025-05-21 16:55:09 +0100

    Setup Factories to create fake data

--- database/factories/JobsFactory.php
+++ database/factories/JobsFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,20 @@ class JobsFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'employer_id' => Employer::factory(),
+            'title' => fake()->jobTitle(),
+            'salary' => fake()->randomElement([
+                '£10,000 - £40,000',
+                '£40,001 - £80,000',
+                '£80,001 - £120,000',
+                '£120,001 - £160,000',
+                '£160,001 - £200,000',
+                '£200,001 - £240,000',
+            ]),
+            'location' => fake()->city(),
+            'schedule' => fake()->randomElement(['Full-time', 'Part-time', 'Contract']),
+            'url' => fake()->url(),
+            'featured' => fake()->boolean(),
         ];
     }
 }

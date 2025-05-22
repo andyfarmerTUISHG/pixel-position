03ec589c10a914ca20cfa0549a293d7e401ddb8c
Andy Farmer <akf0676@sky.com>
2025-05-22 15:19:23 +0100

    Start the Login and Register Process

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use App\Http\Controllers\JobsController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobsController::class, 'index']);
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);

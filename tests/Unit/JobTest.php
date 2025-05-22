93f8087ef20d15c4c0c981c51a5b0171790ba3a1
Andy Farmer <akf0676@sky.com>
2025-05-22 10:02:39 +0100

    TDD - Test JOb Belongs to employer

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -3,6 +3,11 @@
 use App\Models\Employer;
 use App\Models\Jobs;
 
-it ('andy first pest test', function () {
-    expect(true)->toBeTrue();
+it('belongs to an employer', function () {
+    $employer = Employer::factory()->create();
+    $job = Jobs::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    expect($job->employer->is($employer))->toBeTrue();
 });

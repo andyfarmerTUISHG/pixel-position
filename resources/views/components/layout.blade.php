7437ba3fb9b184d8cb73f5d81bbb5f3c1ae0818c
Andy Farmer <akf0676@sky.com>
2025-05-22 15:44:29 +0100

    Fix Logout route, and provide dynamic links for auth and non-auth users

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,9 +26,21 @@
                 <a href="/salaries">Salaries</a>
                 <a href="/companies">Companies</a>
             </div>
-            <div>
-                <a href="/post-a-job">Post a Job</a>
-            </div>
+
+                <div class="space-x-6 font-bold">
+                @auth
+                    <a href="/jobs/create">Post a Job</a>
+                    <form action="/logout" method="POST" class="inline">
+                        @csrf
+                        <button type="submit" class="bg-red-500 text-white rounded px-4 py-2 hover:bg-red-600 transition-colors duration-300 cursor-pointer">Logout</button>
+                    </form>
+                @endauth
+                @guest
+                    <a href="/login">Login</a>
+                    <a href="/register">Register</a>
+                @endguest
+                </div>
+
         </nav>
         <main class="mt-10 max-w-[986px] mx-auto">
             {{ $slot }}

e5fc4700a3f7001a2320a0d8a6e4f4310da4c4ff
Andy Farmer <akf0676@sky.com>
2025-05-22 09:54:27 +0100

    Setting TDD - For Jobs
    - Enable Pest (composer require pestphp/pest --dev --with-all-dependencies)
    - Init Pest (./vendor/bin/pest --init)

--- app/Models/Jobs.php
+++ app/Models/Jobs.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Jobs extends Model
 {
     /** @use HasFactory<\Database\Factories\JobsFactory> */
     use HasFactory;
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags() {
+        return [];
+    }
+
+    public function tag() {
+
+    }
 }
---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Employer;
+use App\Models\Jobs;
+
+it ('andy first pest test', function () {
+    expect(true)->toBeTrue();
+});
+
+it('belongs to an employer', function () {
+    $employer = Employer::factory()->create();
+    $job = Jobs::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    expect($job->employer->is($employer))->toBeTrue();
+});
+
+it('can have tags', function () {
+    $job = Jobs::factory()->create();
+
+    $job->tag('Frontend');
+
+    expect($job->tags)->toHaveCount(1);
+});

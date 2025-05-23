c8fdde1f17be48652424252e1a3d750ba791303d
Andy Farmer <akf0676@sky.com>
2025-05-23 09:23:41 +0100

    Enable Jobs Creation in DB - name login route to redirect

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreJobsRequest;
-use App\Http\Requests\UpdateJobsRequest;
 use App\Models\Jobs;
 use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class JobsController extends Controller
 {
@@ -14,7 +16,7 @@ class JobsController extends Controller
      */
     public function index()
     {
-        $jobs = Jobs::all()->groupBy('featured');
+        $jobs = Jobs::latest()->get()->groupBy('featured');
 
         $tags = Tag::all();
         return view('jobs.index', [
@@ -29,46 +31,37 @@ class JobsController extends Controller
      */
     public function create()
     {
-        //
+        return view("jobs.create");
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreJobsRequest $request)
+    public function store(Request $request)
     {
-        //
+        $attributes = $request->validate([
+            "title" => ["required","string","max:255"],
+            "salary" => ["required"],
+            "location" => ["required"],
+            "schedule" => ["required", Rule::in("Full Time", "Part Time")],
+            "url" => ["required","url"],
+            "tags" => ['nullable'],
+        ]);
+
+        $attributes['featured'] = $request->has("featured");
+
+        // Get the logged user employer
+        $job = Auth::user()->employer->jobs()->create(Arr::except($attributes, "tags"));
+
+        //Deal with Tags
+        if ($attributes['tags'] ?? false){
+            foreach(explode(",", $attributes['tags']) as $tag){
+                $job->tag($tag);
+            }
+        }
+
+        return redirect("/");
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Jobs $jobs)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Jobs $jobs)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateJobsRequest $request, Jobs $jobs)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Jobs $jobs)
-    {
-        //
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,16 @@ Route::get('/', [JobsController::class, 'index']);
 Route::get('/search', SearchController::class);
 Route::get('/tags/{tag:name}', TagController::class);
 
+Route::middleware('auth')->group(function () {
+    Route::get('/jobs/create', [JobsController::class, 'create']);
+    Route::post('/jobs', [JobsController::class, 'store']);
+});
+
 Route::middleware('guest')->group(function () {
     Route::get('/register', [RegisteredUserController::class, 'create']);
     Route::post('/register', [RegisteredUserController::class, 'store']);
 
-    Route::get('/login', [SessionController::class, 'create']);
+    Route::get('/login', [SessionController::class, 'create'])->name('login');
     Route::post('/login', [SessionController::class, 'store']);
 });
 
